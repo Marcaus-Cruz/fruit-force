809d802cdbf5169a435658a6ee6c05c53d6963b5
Marcaus.Cruz <marcauscruz@gmail.com>
2025-05-05 12:55:29 -0700

    Change server port for front end development

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
+  },
+  server: {
+    port: 8080,
   },
 });
